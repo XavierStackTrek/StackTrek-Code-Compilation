63de3a6a8ffa16e943461067c399a14e79cc646a
xjex <dev.xjmanaloto@gmail.com>
2023-08-24 15:06:07 +0800

    create table pg -> postgres

--- DICT-SourceCodes/Sprint 6 - NodeJS/create-table.js
+++ DICT-SourceCodes/Sprint 6 - NodeJS/create-table.js
@@ -2,14 +2,19 @@ const sql = require("./db");
 
 async function createTable() {
   try {
-    await sql`
-    CREATE TABLE IF NOT EXISTS users_base (
-      user_id SERIAL PRIMARY KEY,
-      first_name VARCHAR(255) NOT NULL,
-      last_name VARCHAR(255) NOT NULL
-    )
-  `;
-    console.log("Table 'users' created successfully");
+    let noticeMessage = "";
+
+    // Register a handler for NOTICE messages
+
+    const data = await sql`
+      CREATE TABLE IF NOT EXISTS users_base1 (
+        user_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL
+      )
+    `;
+    console.log("Table created successfully!");
+    sql.end();
   } catch (error) {
     console.error("Error creating table:", error);
   }
